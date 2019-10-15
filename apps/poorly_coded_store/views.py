18fa5f52f11aa1bbb71e27a57912b873bc29ea2e
Nick Voulelis <njvoulelis@gmail.com>
2019-10-15 12:32:50 -0600

    first commit

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -8,9 +8,23 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    context = {
+        "order": Order.objects.last()
+        }
+    context['totalOrders'] = 0
+    context['totalMoney'] = 0
+    orders = Order.objects.all()
+    for i in orders:
+        context['totalOrders']+=i.quantity_ordered
+        context['totalMoney']+=i.total_price
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return render(request, "store/checkout.html", context)
+
+def process(request):
+    quantity_from_form = int(request.POST["quantity"])
+    id_from_form = int(request.POST["id"])
+    product = Product.objects.get(id=id_from_form)
+    price_from_form = product.price
+    total_price=quantity_from_form*price_from_form
+    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_price)
+    return redirect("/checkout")
